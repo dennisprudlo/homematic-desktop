aec72f63edc7f2a9be63ce3a8f5076a2613be4a3
Dennis Prudlo <mail@dennisprudlo.com>
2021-12-27 22:35:05 +0100

    Add localization

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createI18n } from 'vue-i18n';
 
-createApp(App).mount('#app')
+import locale_en from './assets/lang/en';
+
+const i18n = createI18n({
+    locale: navigator.language.split('-')[0] || 'en',
+    fallbackLocale: 'en',
+    messages: {
+        en: locale_en
+    }
+});
+
+const app = createApp(App)
+app.use(i18n);
+app.mount('#app')
